49f5ba33e05df98698c354e9b46a64c36236951a
jamshedirmatov2 <jamshedirmatov2@gmail.com>
2024-03-08 16:39:04 +0500

    Notification

--- app/src/main/java/com/wigroup/services/MainActivity.kt
+++ app/src/main/java/com/wigroup/services/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.wigroup.services
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build.VERSION
+import android.os.Build.VERSION_CODES
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.wigroup.services.databinding.ActivityMainBinding
@@ -26,5 +31,34 @@ class MainActivity : AppCompatActivity() {
         binding.simpleService.setOnClickListener {
             startService(MyService.newIntent(this, 25))
         }
+        binding.foregroundService.setOnClickListener { showNotification() }
+    }
+
+    private fun showNotification() {
+        println("showNotification")
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+
+        if (VERSION.SDK_INT >= VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT,
+            )
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle("Title")
+            .setContentText("Text")
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .build()
+
+        notificationManager.notify(NOTIFY_ID, notification)
+    }
+
+    companion object {
+        const val CHANNEL_ID = "service"
+        const val CHANNEL_NAME = "Service"
+        const val NOTIFY_ID = 1
     }
 }
