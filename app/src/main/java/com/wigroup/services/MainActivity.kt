45df332be1725a98522c77a81cd233e7e7086ef6
jamshedirmatov2 <jamshedirmatov2@gmail.com>
2024-03-08 20:10:42 +0500

    Job service more

--- app/src/main/java/com/wigroup/services/MainActivity.kt
+++ app/src/main/java/com/wigroup/services/MainActivity.kt
@@ -2,7 +2,9 @@ package com.wigroup.services
 
 import android.app.job.JobInfo
 import android.app.job.JobScheduler
+import android.app.job.JobWorkItem
 import android.content.ComponentName
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -16,6 +18,8 @@ class MainActivity : AppCompatActivity() {
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+
+    private var page = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,11 +52,14 @@ class MainActivity : AppCompatActivity() {
             val jobInfo = JobInfo.Builder(MyJobService.JOB_ID, componentName)
                 .setRequiresCharging(true)
                 .setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED)
-                .setPersisted(true)
                 .build()
 
             val jobScheduler = getSystemService(JOB_SCHEDULER_SERVICE) as JobScheduler
-            jobScheduler.schedule(jobInfo)
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val intent = MyJobService.newIntent(page++)
+                jobScheduler.enqueue(jobInfo, JobWorkItem(intent))
+            }
         }
     }
 }
--- app/src/main/java/com/wigroup/services/MyJobService.kt
+++ app/src/main/java/com/wigroup/services/MyJobService.kt
@@ -2,6 +2,8 @@ package com.wigroup.services
 
 import android.app.job.JobParameters
 import android.app.job.JobService
+import android.content.Intent
+import android.os.Build
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.cancel
@@ -20,11 +22,20 @@ class MyJobService : JobService() {
     override fun onStartJob(params: JobParameters?): Boolean {
         println("onStartJob")
         coroutineScope.launch {
-            for (i in 0 until 100) {
-                delay(1000)
-                println("Timer: $i")
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                var workItem = params?.dequeueWork()
+                while (workItem != null) {
+                    val page = workItem.intent.getIntExtra(PAGE, 0)
+
+                    for (i in 0 until 5) {
+                        delay(1000)
+                        println("Timer: $i, Page: $page")
+                    }
+                    params?.completeWork(workItem)
+                    workItem = params?.dequeueWork()
+                }
+                jobFinished(params, false)
             }
-            jobFinished(params, true)
         }
         return true
     }
@@ -42,5 +53,10 @@ class MyJobService : JobService() {
 
     companion object {
         const val JOB_ID = 1
+        private const val PAGE = "page"
+
+        fun newIntent(page: Int) = Intent().apply {
+            putExtra(PAGE, page)
+        }
     }
 }
